128da48167a7319bca31e0e8cec9ae0d752dd769
superjdg <42200193+superjdg@users.noreply.github.com>
2023-10-02 21:07:01 +0800

    Update MUSIC_sparse.m

--- CRB_test/MUSIC_sparse.m
+++ CRB_test/MUSIC_sparse.m
@@ -1,4 +1,4 @@
-% 20221209 comb-filter of block snapshot matrix
+% 20221209
 %@article{wang2023min, 
 % title={Min-max Probe Placement and Extended Relaxation Estimation Method for Processing Blade Tip Timing Signals}, 
 % author={Wang, Zengkun and Petre, Stoica and Dave, Zachariah and Prabhu, Babu and Zhibo, Yang}, 
